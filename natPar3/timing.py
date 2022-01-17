adc9a2c78bed6f64ff1a6289dc1b355b3e4da205
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-01-17 06:33:10 +0100

    multiple inputs per run

--- natPar3/timing.py
+++ natPar3/timing.py
@@ -1,28 +1,33 @@
 import timeit
+import time
 import importlib
+import argparse
 import pandas as pd
 from statistics import median, geometric_mean
 
 from helpers import library_proxy
-from helpers.timing_utils import get_argument_parser, Version, lib_select
+from helpers.timing_utils import default_libraries, Version, lib_select
 
 import natPar3.sequential as sequential
 import natPar3.compiled as compiled
-# import natPar3.as_pool as as_pool
 import natPar3.algo as algo
+import natPar3.threaded as threaded
+import natPar3.as_pool as pooled
 
-
+"""
 versions = [Version("sequential", sequential, []),
-            Version("compiled", compiled, [algo])]
+            Version("compiled", compiled, [algo]),
+            Version("threaded", threaded, []),
+            Version("pooled", pooled, [])]"""
+versions = [Version("threaded", threaded, []),
+            Version("pooled", pooled, [])]
 
 
-def take_times(problemSize, reps, lib_arg= None, pname=__name__):
-    global pSize
-    pSize = problemSize
+def take_times(inputs, reps, lib_arg= None, pname=__name__):
+    global input
 
     libraries = [lib_arg] if lib_arg \
         else [name for name in lib_select.keys()]
-
     all_measurements = []
 
     for version in versions:
@@ -33,33 +38,66 @@ def take_times(problemSize, reps, lib_arg= None, pname=__name__):
             # reload the module to link the new library
             importlib.reload(version.module)
             [importlib.reload(mod) for mod in version.dependent_modules]
-
-            test_params = [pname, version.name,
-                           library, pSize, reps]
-            setup = "from {} import pSize, library_proxy, {};" \
-                .format(__name__, version.name)
-            times = timeit.Timer(stmt="{}.algo(pSize)".format(version.name),
-                                 setup=setup) \
-                .repeat(reps, number=1)
-            print("{} with {} done in {}".format(version.name, library,
-                                                 median(times)))
-            measurements = [min(times), geometric_mean(times), max(times)]
-            all_measurements.append(test_params + measurements)
+            for input in inputs:
+                test_params = [pname, version.name,
+                               library, input, reps]
+                setup = "from {} import input, library_proxy, {};" \
+                    .format(__name__, version.name)
+                times = timeit.Timer(stmt="{}.algo(input)".format(version.name),
+                                     setup=setup) \
+                    .repeat(reps, number=1)
+                print("{} with {} done in {}".format(version.name, library,
+                                                     median(times)))
+                measurements = [min(times), geometric_mean(times), max(times)]
+                all_measurements.append(test_params + measurements)
     return all_measurements
 
+
 def main(args):
-    pSize = args.Input
+    inputs = args.inputs
     reps = args.repetitions
     lib = args.library
-    measurements = take_times(pSize, reps, lib, "natPar3")
+    measurements = take_times(inputs, reps, lib, "natPar3")
     # prepare data collection
     data = pd.DataFrame(
         columns=["scenario", "version", "library", "input",
                  "reps", "min", "geo-mean", "max"],
         data=measurements)
+    # Seconds to milliseconds
+    data[["min", "geo-mean", "max"]] *= 1000
     data.to_csv(args.output)
 
 
+def get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--inputs',
+        nargs='+',
+        type=int,
+        required=True,
+        help="integer inputs to the algorithm. Many inputs -> Many tests "
+    )
+    parser.add_argument(
+        "-r", "--repetitions",
+        type=int,
+        default=10,
+        help="how often to repeat the timing",
+    )
+    parser.add_argument(
+        "-l", "--library",
+        type=str,
+        default=None,
+        help="which library to take the functions from. Options: {}"
+            .format(default_libraries),
+    )
+    parser.add_argument(
+        "-o", "--output",
+        type=str,
+        default="./time_measures_{}.csv".format(
+            time.strftime("%Y_%m_%d_%I_%M")),
+        help="file path to write the timing measurements to  ",
+    )
+    return parser
+
 if __name__ == '__main__':
     main(get_argument_parser().parse_args())
-
--- timeCases.py
+++ timeCases.py
@@ -1,35 +1,66 @@
+import argparse
+import time
 import pandas as pd
 import helpers.timing_utils as timing_utils
 import natPar3.timing
 import natPar7.timing
 import natPar15.timing
-import natPar32.timing
-from natPar7 import timing as timing7
+import natPar31.timing
 case_selector = {"natPar3": natPar3.timing,
                 "natPar7": natPar7.timing,
                 "natPar15": natPar15.timing,
-                "natPar32": natPar32.timing}
+                "natPar31": natPar31.timing}
+
 
 
 def main(args):
-    pSize = args.Input
+    inputs = args.inputs
     reps = args.repetitions
     lib = args.library
     test_cases = [case_selector[args.Case]] if args.Case \
         else list(case_selector.values())
     all_measurements = []
     for case in test_cases:
-        measurements = case.take_times(pSize, reps, lib)
+        measurements = case.take_times(inputs, reps, lib)
         all_measurements.extend(measurements)
     data = pd.DataFrame(
         columns=["scenario", "version", "library", "input",
                  "reps", "min", "geo-mean", "max"],
         data=all_measurements)
+    # Seconds to milliseconds
+    data[["min", "geo-mean", "max"]] *= 1000
     data.to_csv(args.output)
 
 
 def _get_argument_parser():
-    parser = timing_utils.get_argument_parser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--inputs",
+        nargs='+',
+        required=True,
+        type=int,
+        help="integer inputs to the algorithm. Many inputs -> Many tests "
+    )
+    parser.add_argument(
+        "-r", "--repetitions",
+        type=int,
+        default=10,
+        help="how often to repeat the timing",
+    )
+    parser.add_argument(
+        "-l", "--library",
+        type=str,
+        default=None,
+        help="which library to take the functions from. Options: {}"
+            .format(timing_utils.default_libraries),
+    )
+    parser.add_argument(
+        "-o", "--output",
+        type=str,
+        default="./time_measures_{}.csv".format(
+            time.strftime("%Y_%m_%d_%I_%M")),
+        help="file path to write the timing measurements to  ",
+    )
     parser.add_argument(
         "-c", "--Case",
         type=str,
