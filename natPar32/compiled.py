b7f402a8ea4acdeafac2c5b00de42998a69c3721
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-02-03 17:50:08 +0100

    undo temp change

--- natPar32/compiled.py
+++ natPar32/compiled.py
@@ -1,5 +1,5 @@
-#import natPar32.algo as algo_parallel
-import natPar32.algo_reodered as algo_parallel
+import natPar32.algo as algo_parallel
+# import natPar32.algo_reodered as algo_parallel
 
 def algo(i):
     return algo_parallel.main(i)
